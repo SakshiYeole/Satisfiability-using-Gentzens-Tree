181a8ee70448c7faab05c0706c1452c2c802ca91
Sakshi Yeole <sakshiyeole210@gmail.com>
2023-09-24 16:07:35 +0530

    Modified String constanst

--- source/model/StringConstants.py
+++ source/model/StringConstants.py
@@ -1,5 +1,23 @@
-disjunction = '\u22C0'
-conjunction = '\u22C1'
-implication = '\u2192'
-double_implication = '\u2194'
-negation  = '\u223c'
+
+class StringOperators:    
+    disjunction = '\u22C1'
+    conjunction = '\u22C0'
+    implication = '\u2192'
+    double_implication = '\u2194'
+    negation  = '\u223c'
+
+    @staticmethod
+    def is_operator(str_value):
+        operators = [StringOperators.implication, StringOperators.double_implication, StringOperators.disjunction, StringOperators.conjunction, StringOperators.negation]
+        return str_value in operators
+    
+    @staticmethod
+    def precedence_of_operators(operator):
+        precedence = { 
+            StringOperators.negation: 1,
+            StringOperators.disjunction: 2,
+            StringOperators.conjunction: 3,
+            StringOperators.implication: 4,
+            StringOperators.double_implication: 5 
+        }
+        return precedence.get(operator, -1)
