210a580d0ca183ab6d5e060d0d7690d829088cc2
Sakshi Yeole <sakshiyeole210@gmail.com>
2023-09-25 02:49:36 +0530

    Added class GentzensTree and its functionalities

---
+++ source/model/GentzensTree.py
@@ -0,0 +1,64 @@
+from collections import deque
+from model import Node
+class GentzensTree:
+    def __init__(self, expression) -> None:
+        LHS = []
+        RHS = []
+        RHS.append(expression)
+
+        self.root_node = Node.Node(LHS, RHS)
+        self.root_node.strip_left_and_right_handside()
+        self.leaf_nodes = []
+
+    def algorithm_for_gentzen_system_creating_tree(self):
+        unserved_nodes = deque()
+        unserved_nodes.append(self.root_node)
+
+        while unserved_nodes:
+            curr_node = unserved_nodes.popleft()
+
+            new_nodes_for_curr = curr_node.get_new_nodes()
+            assert len(new_nodes_for_curr) <= 2
+
+            if len(new_nodes_for_curr) == 0 :
+                self.leaf_nodes.append(curr_node)
+            elif len(new_nodes_for_curr) == 1:
+                curr_node.set_left_child(new_nodes_for_curr[0])
+            elif len(new_nodes_for_curr) == 2:
+                curr_node.set_left_child(new_nodes_for_curr[0])
+                curr_node.set_right_child(new_nodes_for_curr[1])
+
+            unserved_nodes.append(new_nodes_for_curr)
+
+    def print_tree(self):
+        print("Level Order Traversal of the Gentzen's Proof Tree: ")
+        q = deque()
+        q.append(self.root_node)
+
+        while q:
+            curr = q.popleft()
+            print(curr)
+            print()
+
+            if curr.get_left_child() is not None:
+                q.append(curr.get_left_child())
+            
+            if curr.get_right_child() is not None:
+                q.append(curr.get_right_child())
+
+    def contradicting_node(self):
+        for node in self.leaf_nodes:
+            if node.is_contradiction():
+                return node        
+        return None
+    
+    def print_contradicting_node(self):
+        print(f"Node with contradiction: {self.contradicting_node()}")
+
+    def check_contradiction(self):
+        return self.contradicting_node() is not None
+    
+    def check_satisfiability(self):
+        return self.check_contradiction is not None
+
+        
